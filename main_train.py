all commend done TODO: -read sequence example -implement dynamic_pad to speed up rnn computation -fix eval dimentions

main_train.py
@@ -15,31 +15,32 @@ tf.flags.DEFINE_string("model_dir", './runs_{}'.format(TIMESTAMP), "Directory to
 tf.flags.DEFINE_integer("loglevel", 20, "Tensorflow log level")
 tf.flags.DEFINE_integer("num_epochs", 2000, "Number of training Epochs. Defaults to indefinite.")
 tf.flags.DEFINE_integer("eval_every", 100, "Evaluate after this many train steps")
+tf.flags.DEFINE_float("memory_fraction", 0., "fraction of gpu memory allocated")
 FLAGS = tf.flags.FLAGS
 
 
-TRAIN_FILE = FLAGS.input_dir + '/train_my.tfrecords'
-VALIDATION_FILE =  FLAGS.input_dir + 'validation_my.tfrecords'
+TRAIN_FILE = FLAGS.input_dir + '/train.tfrecords'
+VALIDATION_FILE =  FLAGS.input_dir + '/validation.tfrecords'
 
 tf.logging.set_verbosity(FLAGS.loglevel)
 
 def main():
     hparams = network_params.create_hparams()
 
-    model_fn = model.create_model_fn(
+    model_fn = model.create_model_fn(           # create the model
         hparams,
         model_impl=dual_encoder_model)
 
     estimator = tf.contrib.learn.Estimator(     # creation of the estimator for our model_fn functions
-        model_fn=model_fn,
-        model_dir=FLAGS.model_dir,
-        config=tf.contrib.learn.RunConfig())
+        model_fn=model_fn,                      # model function
+        model_dir=FLAGS.model_dir,              # directory to save the model paramters, graphs, etc.
+        config=tf.contrib.learn.RunConfig(gpu_memory_fraction=FLAGS.memory_fraction))   # specify the ammount of memory to use for the GPU
 
     input_fn_train = IO_data.create_input_fn(   # ensure that the model receive the data in the correct format for training
         mode=tf.contrib.learn.ModeKeys.TRAIN,
         input_files=[TRAIN_FILE],
         batch_size=hparams.batch_size,
-        num_epochs=FLAGS.num_epochs)
+        num_epochs=FLAGS.num_epochs)            # Integer specifying the number of times to read through the dataset.
 
     input_fn_eval = IO_data.create_input_fn(    # ensure that the model receive the data in the correct format for evaluation
         mode=tf.contrib.learn.ModeKeys.EVAL,

model.py
@@ -8,24 +8,32 @@ def get_id_feature(features, key, len_key, max_len):
     return ids, ids_len
 
 def create_train_op(loss, hparams):
+    '''
+    Function used to train the model
+    :param loss: loss function to evaluate the error
+    :param hparams: hiper-parameters used to configure the optimizer
+    :return: training updates
+    '''
     train_op = tf.contrib.layers.optimize_loss(
-        loss=loss,
-        global_step=tf.contrib.framework.get_global_step(),
-        learning_rate=hparams.learning_rate,
-        clip_gradients=10.0,
-        optimizer=hparams.optimizer)
+        loss=loss,                                          # loss function used
+        global_step=tf.contrib.framework.get_global_step(), # number of batches seen so far
+        learning_rate=hparams.learning_rate,                # learning rate
+        clip_gradients=10.0,                                # clip gradient to a max value
+        optimizer=hparams.optimizer)                        # optimizer used
     return train_op
 
 
 def create_model_fn(hparams, model_impl):
+    '''
+    Function used to create the model according different implementations and usage mode
+    :param hparams: hiper-parameters used to configure the model
+    :param model_impl: implementation of the model used, have to use the same interface to inject a different model
+    :return: probabilities of the predicted class, value of the loss function, operation to execute the training
+    '''
 
     def model_fn(features, targets, mode):
-        context, context_len = get_id_feature(
-            features, "context", "context_len", hparams.max_context_len)
-
-        utterance, utterance_len = get_id_feature(
-            features, "utterance", "utterance_len", hparams.max_utterance_len)
-
+        context, context_len = get_id_feature(features, "context", "context_len", hparams.max_context_len)
+        utterance, utterance_len = get_id_feature(features, "utterance", "utterance_len", hparams.max_utterance_len)
         batch_size = targets.get_shape().as_list()[0]
 
         if mode == tf.contrib.learn.ModeKeys.TRAIN:
@@ -58,7 +66,7 @@ def create_model_fn(hparams, model_impl):
             all_context_lens = [context_len]
             all_utterances = [utterance]
             all_utterance_lens = [utterance_len]
-            all_targets = [tf.ones([batch_size, 1], dtype=tf.int64)]
+            all_targets = [tf.ones([batch_size, 1], dtype=tf.int64)]            # all utterance have positive labels
 
             for i in range(9):
                 distractor, distractor_len = get_id_feature(features,
@@ -70,26 +78,26 @@ def create_model_fn(hparams, model_impl):
                 all_utterances.append(distractor)
                 all_utterance_lens.append(distractor_len)
                 all_targets.append(
-                    tf.zeros([batch_size, 1], dtype=tf.int64)
+                    tf.zeros([batch_size, 1], dtype=tf.int64)                   # all distractor have negative labels
                 )
 
             probs, loss = model_impl(
                 hparams,
                 mode,
-                tf.concat(0, all_contexts),
-                tf.concat(0, all_context_lens),
-                tf.concat(0, all_utterances),
-                tf.concat(0, all_utterance_lens),
-                tf.concat(0, all_targets))
+                tf.concat(all_contexts, axis=0),
+                tf.concat(all_context_lens, 0),
+                tf.concat(all_utterances, axis=0),
+                tf.concat(all_utterance_lens, axis=0),
+                tf.concat(all_targets, axis=0))
 
-            split_probs = tf.split(0, 10, probs)
-            shaped_probs = tf.concat(1, split_probs)
+            split_probs = tf.split(probs, num_or_size_splits=10, axis=0)    # split the probabilities between the first positive utterance and the following 9 negative distractors
+            shaped_probs = tf.concat(split_probs, axis=1)                   # matrix with shape(?, 10) for each example we have the probability of each response
 
             # Add summaries
-            tf.histogram_summary("eval_correct_probs_hist", split_probs[0])
-            tf.scalar_summary("eval_correct_probs_average", tf.reduce_mean(split_probs[0]))
-            tf.histogram_summary("eval_incorrect_probs_hist", split_probs[1])
-            tf.scalar_summary("eval_incorrect_probs_average", tf.reduce_mean(split_probs[1]))
+            tf.summary.histogram("eval_correct_probs_hist", split_probs[0])
+            tf.summary.scalar("eval_correct_probs_average", tf.reduce_mean(split_probs[0]))
+            tf.summary.histogram("eval_incorrect_probs_hist", split_probs[1])
+            tf.summary.scalar("eval_incorrect_probs_average", tf.reduce_mean(split_probs[1]))
 
             return shaped_probs, loss, None
 

models/dual_encoder.py
@@ -28,13 +28,6 @@ def get_embeddings(hparams):
 
 
 def dual_encoder_model(hparams, mode, context, context_len, utterance, utterance_len, targets):
-    # Logic to do the following:
-    # 1. Configure the model via TensorFlow operations
-    # 2. Define the loss function for training/evaluation
-    # 3. Define the training operation/optimizer
-    # 4. Generate predictions
-    # 5. Return predictions/loss/train_op/eval_metric_ops in ModelFnOps object
-
     # Initialize embedidngs randomly or with pre-trained vectors if available
     embeddings_W = get_embeddings(hparams)
 
@@ -53,11 +46,11 @@ def dual_encoder_model(hparams, mode, context, context_len, utterance, utterance
             state_is_tuple=True)
 
         # Run the utterance and context through the RNN
-        rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell,
-                                                    tf.concat([context_embedded, utterance_embedded], axis=0),
-                                                    sequence_length=tf.concat([context_len, utterance_len], axis=0),
+        rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell,                                                               # rnn_output[?, sequence_length, RNN_dim] => output of the rnn at each timestamp(for each word), rnn_state[?, RNN_DIM] => the last state for each example(sentence)
+                                                    tf.concat([context_embedded, utterance_embedded], axis=0),          # encode all the context and then all the utterance of the current batch
+                                                    sequence_length=tf.concat([context_len, utterance_len], axis=0),    # specify the length of each embdding to early stop
                                                     dtype=tf.float32)
-        encoding_context, encoding_utterance = tf.split(rnn_states.h, num_or_size_splits=2, axis=0)
+        encoding_context, encoding_utterance = tf.split(rnn_states.h, num_or_size_splits=2, axis=0)                     # since we have the same amount of context and utterance, split the final output in half will give us the encoding of the contexts and of the utterances
 
     with tf.variable_scope("prediction") as vs:
         M = tf.get_variable("M",
@@ -66,13 +59,14 @@ def dual_encoder_model(hparams, mode, context, context_len, utterance, utterance
 
         # "Predict" a  response: c * M
         generated_response = tf.matmul(encoding_context, M)             # execute a linear classification
+        # expand_dim used to guarantee the correct broadcasting during the computation of the logits
         generated_response = tf.expand_dims(generated_response, 2)      # add a dimention at the end of the shape, generate_response.shape => [batch_size, rnn_dim, 1]
-        encoding_utterance = tf.expand_dims(encoding_utterance, 2)
+        encoding_utterance = tf.expand_dims(encoding_utterance, 2)      # add a dimention at the end of the shape, encoding_utterance.shape => [batch_size, rnn_dim, 1]
 
         # Dot product between generated response and actual response
         # (c * M) * r
         logits = tf.matmul(generated_response, encoding_utterance, transpose_a=True)
-        logits = tf.squeeze(logits, [2])        # remove 3D dimention
+        logits = tf.squeeze(logits, [2])        # remove 3D dimention,  logits.shape => [batch_size, 1]
 
         # Apply sigmoid to convert logits to probabilities
         probs = tf.sigmoid(logits)

utils/IO_data.py
@@ -103,14 +103,14 @@ def create_input_fn(mode, input_files, batch_size, num_epochs):
     '''
     def input_fn():
         features = tf.contrib.layers.create_feature_spec_for_parsing(get_feature_columns(mode))
-
+        # TODO: use the sequenceExample input and dynamic_pad flag
         feature_map = tf.contrib.learn.io.read_batch_features(  # read the data form Example protocol files. Only the specified features are readed
             file_pattern=input_files,
             batch_size=batch_size,
             features=features,
             reader=tf.TFRecordReader,
             randomize_input=True,
-            num_epochs=num_epochs,
+            num_epochs=num_epochs,                              # Integer specifying the number of times to read through the dataset.
             queue_capacity=200000 + batch_size * 10,
             name="read_batch_features_{}".format(mode))
 

utils/network_params.py
@@ -8,7 +8,7 @@ tf.flags.DEFINE_integer("vocab_size", 91620, "The size of the vocabulary. Only c
 tf.flags.DEFINE_integer("embedding_dim", 100, "Dimensionality of the embeddings")
 tf.flags.DEFINE_integer("rnn_dim", 256, "Dimensionality of the RNN cell")
 tf.flags.DEFINE_integer("max_context_len", 160, "Truncate contexts to this length")
-tf.flags.DEFINE_integer("max_utterance_len", 80, "Truncate utterance to this length")
+tf.flags.DEFINE_integer("max_utterance_len", 160, "Truncate utterance to this length")
 
 # Pre-trained embeddings
 tf.flags.DEFINE_string("glove_path", 'glove.6B.100d.txt', "file name to pre-trained Glove vectors")